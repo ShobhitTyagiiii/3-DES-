3dc7de8b9118ec9395a01ddfcb2413924bb55cc7
ShobhitTyagiiii <155055575+ShobhitTyagiiii@users.noreply.github.com>
2025-01-29 10:29:37 +0530

    Update IS.py

--- IS.py
+++ IS.py
@@ -4,41 +4,24 @@ from Crypto.Hash import SHA256
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Util.Padding import pad, unpad
 import os
-from io import BytesIO
 
-
-KEY_LENGTH = 24  
-SALT = b"$ez*&214097GDAKACNASC;LSOSSBAdjskasnmosuf!@#$^()_adsa"  
+# Constants
+KEY_LENGTH = 24
+SALT = b"$ez*&214097GDAKACNASC;LSOSSBAdjskasnmosuf!@#$^()_adsa"
 
 
 def encryptor(image_data, password):
     try:
-      
         padded_data = pad(image_data, DES.block_size)
-
-     
         hash_original = SHA256.new(data=image_data)
-
-        
         key = PBKDF2(password, SALT, dkLen=KEY_LENGTH)
-
-
-        iv1 = os.urandom(8)  
-        iv2 = os.urandom(8) 
-        iv3 = os.urandom(8)  
+        iv1, iv2, iv3 = os.urandom(8), os.urandom(8), os.urandom(8)
 
         cipher1 = DES.new(key[:8], DES.MODE_CBC, iv1)
         cipher2 = DES.new(key[8:16], DES.MODE_CBC, iv2)
         cipher3 = DES.new(key[16:], DES.MODE_CBC, iv3)
 
-       
-        encrypted_data = cipher3.encrypt(
-            cipher2.decrypt(
-                cipher1.encrypt(padded_data)
-            )
-        )
-
-        
+        encrypted_data = cipher3.encrypt(cipher2.decrypt(cipher1.encrypt(padded_data)))
         encrypted_data_with_iv = iv1 + iv2 + iv3 + hash_original.digest() + encrypted_data
 
         return encrypted_data_with_iv, None
@@ -48,30 +31,18 @@ def encryptor(image_data, password):
 
 def decryptor(encrypted_data_with_iv, password):
     try:
-      
         iv1, iv2, iv3 = encrypted_data_with_iv[:8], encrypted_data_with_iv[8:16], encrypted_data_with_iv[16:24]
         extracted_hash = encrypted_data_with_iv[24:56]
         encrypted_data = encrypted_data_with_iv[56:]
-
-    
         key = PBKDF2(password, SALT, dkLen=KEY_LENGTH)
 
-       
         cipher1 = DES.new(key[:8], DES.MODE_CBC, iv1)
         cipher2 = DES.new(key[8:16], DES.MODE_CBC, iv2)
         cipher3 = DES.new(key[16:], DES.MODE_CBC, iv3)
 
-       
-        decrypted_data = cipher1.decrypt(
-            cipher2.encrypt(
-                cipher3.decrypt(encrypted_data)
-            )
-        )
-
-       
+        decrypted_data = cipher1.decrypt(cipher2.encrypt(cipher3.decrypt(encrypted_data)))
         decrypted_data = unpad(decrypted_data, DES.block_size)
 
-        
         hash_decrypted = SHA256.new(data=decrypted_data)
         if hash_decrypted.digest() == extracted_hash:
             return decrypted_data, None
@@ -84,19 +55,14 @@ def decryptor(encrypted_data_with_iv, password):
 
 def main():
     st.title("Image Encryption and Decryption using 3DES")
-    
-    
+
     option = st.selectbox("Choose an option", ["Encrypt Image", "Decrypt Image"])
-    
+
     if option == "Encrypt Image":
         uploaded_file = st.file_uploader("Upload an image to encrypt", type=["jpg", "png", "jpeg"])
-        
-        if uploaded_file is not None:
+        if uploaded_file:
             image_data = uploaded_file.read()
-
-            
             password = st.text_input("Enter password for encryption", type="password")
-            
             if st.button("Encrypt"):
                 if len(password) < 8:
                     st.error("Password must be at least 8 characters long.")
@@ -105,7 +71,6 @@ def main():
                     if error:
                         st.error(error)
                     else:
-                      
                         st.success("Encryption successful!")
                         st.download_button(
                             label="Download Encrypted Image",
@@ -113,16 +78,12 @@ def main():
                             file_name="encrypted_image.enc",
                             mime="application/octet-stream"
                         )
-    
+
     elif option == "Decrypt Image":
         encrypted_file = st.file_uploader("Upload an encrypted image", type=["enc"])
-
-        if encrypted_file is not None:
+        if encrypted_file:
             encrypted_data_with_iv = encrypted_file.read()
-
-            
             password = st.text_input("Enter password for decryption", type="password")
-            
             if st.button("Decrypt"):
                 if len(password) < 8:
                     st.error("Password must be at least 8 characters long.")
@@ -132,8 +93,8 @@ def main():
                         st.error(error)
                     else:
                         st.success("Decryption successful!")
-                        
                         st.image(decrypted_data)
-  
+
+
 if __name__ == "__main__":
     main()
